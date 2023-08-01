b119170dc73e4b802fffd58a09104a10f8b09ae2
Luigi Souza <luigisouzasc@gmail.com>
2023-08-01 17:51:54 -0300

    migration of products complete

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/',(request,response) => {
-  return response.json({message: 'hello dev'})
-})
+routes.get('/', (request, response) => {
+  return response.json({ message: 'hello dev' });
+});
 export default routes;
--- src/shared/typeorm/migrations/1690748611825-CreateProducts.ts
+++ src/shared/typeorm/migrations/1690748611825-CreateProducts.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateProducts1690748611825 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.clearTable(
+    await queryRunner.createTable(
       new Table({
         name: 'products',
         columns: [
@@ -13,10 +13,36 @@ export class CreateProducts1690748611825 implements MigrationInterface {
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
 }
