9fbf59e8170b5fb973eaf4fb5dde0a41dac248f0
Luigi Souza <luigisouzasc@gmail.com>
2023-08-09 18:07:25 -0300

    users controller/services/migration complete

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByName(email);
+
+    if (emailExists) {
+      throw new AppError('There is already an user with the same email');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import { errors } from 'celebrate';
 import '@shared/typeorm';
 
 const app = express();
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
